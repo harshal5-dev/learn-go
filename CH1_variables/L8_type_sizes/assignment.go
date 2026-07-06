300fd8fbf4697fd9d995162b0fdb61c8ed8a6281
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-07-06 19:36:49 +0530

    add assignment files for variable declarations and examples

---
+++ CH1_variables/L8_type_sizes/assignment.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	accountAgeFloat := 2.6
+	accountAgeInt := int64(accountAgeFloat)
+
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
+}
